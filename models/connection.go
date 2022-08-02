ee9c888facf0805ca381e138a468c4615317eeb5
ishangoyal13 <ishan0359.cse19@chitkara.edu.in>
2022-08-02 11:10:11 +0530

    minor changes

--- models/connection.go
+++ models/connection.go
@@ -7,10 +7,12 @@ import (
 )
 
 type Blog struct {
-	ID      uint    `gorm:"primary key:autoIncrement" json:"id"`
-	Author  *string `json:"author"`
-	Title   *string `json:"title"`
-	Content *string `json:"content"`
+	ID        uint    `gorm:"primary key:autoIncrement" json:"id"`
+	Author    *string `json:"author"`
+	Title     *string `json:"title"`
+	Content   *string `json:"content"`
+	UserRefer int     `json:"user_id"`
+	User      User    `gorm:"foreignKey:UserRefer"`
 }
 
 var DB *gorm.DB
--- views/js/app.jsx
+++ views/js/app.jsx
@@ -14,8 +14,22 @@ class Home extends React.Component {
         <div className="col-xs-8 col-xs-offset-2 jumbotron text-center">
           <h2>Blog App</h2>
           <p>Sign in to get access </p>
-          <a onClick={this.authenticate}  className="btn btn-primary btn-lg btn-login btn-block mb-5">Sign In</a>
+          <a onClick={this.authenticate} className="btn btn-primary btn-lg btn-login btn-block mb-5">Sign In</a>
         </div>
+      </div>
+    )
+  }
+}
+class Blog extends React.Component {
+  render() {
+    return (
+      <div class="card" style="width: 18rem;">
+        {/* <img class="card-img-top" src="..." alt="Card image cap"> */}
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
       </div>
     )
   }
